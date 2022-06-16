232f64f7299030f80597a74f998a7d4e0f950178
Leonardo Aguiar <leonardoaguiar1235@gmail.com>
2022-06-15 22:15:17 -0300

    React

--- src/pages/Alunos/index.jsx
+++ src/pages/Alunos/index.jsx
@@ -14,29 +14,48 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Container from '@mui/material/Container';
 import axios from "axios";
+import { useEffect, useState } from 'react';
+import Lottie from 'react-lottie';
+import carregandoAnimacao from '../../animations/loading.json';
+import { MyLink } from "../../Styles";
 
 const Alunos = () => {
-    const alunos = [
-        {
-            nome: "Leonardo",
-            idade: 27,
-            id: 1
-        },
-        {
-            nome: "Geraldo",
-            idade: 70,
-            id: 2
-        }
-    ];
+    const [alunos, setAlunos] = useState([]);
+    const [carregando, setCarregando] = useState(true);
 
-    axios.get("https://randomuser.me/api/?results=5").then(({data}) => {
-        console.log(data.results)
-    });
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: carregandoAnimacao,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice",
+        },
+      };
+
+
+
+    // GET - pegarm uma informação
+    // POST - cadastar/ criar uma informação
+    // PUT - alterar/ editar uma informação
+    // DELETE - deletar uma informação
+    useEffect(() => {
+        axios.get("https://randomuser.me/api/?results=5").then(({ data }) => {
+            setAlunos(data.results);
+        });
+    }, []);
+
+    useEffect(() => {
+        if (alunos.length > 0) {
+            setTimeout(() => {
+                setCarregando(false);
+            }, 3000);
+        }
+    }, [alunos]);
 
     return (
         <>
             <Box sx={{ flexGrow: 1 }}>
-                <AppBar position="static" sx={{background: "#ff5555"}}>
+                <AppBar position="static" sx={{ background: "#ff5555" }}>
                     <Toolbar>
                         <IconButton
                             size="large"
@@ -50,35 +69,46 @@ const Alunos = () => {
                         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                             News
                         </Typography>
-                        <Button color="inherit">Login</Button>
+                        <Button color="inherit">
+              <MyLink to="/materias/cadastro">Login</MyLink>
+            </Button>
                     </Toolbar>
                 </AppBar>
             </Box>
 
             <Container maxWidth="sm">
-                <TableContainer component={Paper} sx={{ mt: 3 }}>
-                    <Table size="small" aria-label="a dense table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell align="right">Id</TableCell>
-                                <TableCell align="right">Nome</TableCell>
-                                <TableCell align="right">Idade</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {alunos.map((aluno) => (
-                                <TableRow
-                                    key={aluno.id}
-                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                >
-                                    <TableCell align="right">{aluno.id}</TableCell>
-                                    <TableCell align="right">{aluno.nome}</TableCell>
-                                    <TableCell align="right">{aluno.idade}</TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
+                {
+                    carregando ? (   <Lottie 
+                        options={defaultOptions}
+                        height={400}
+                        width={400} speed={2}
+                      />) : (
+                        <TableContainer component={Paper} sx={{ mt: 3 }}>
+                            <Table size="small" aria-label="a dense table">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell align="right">Id</TableCell>
+                                        <TableCell align="right">Nome</TableCell>
+                                        <TableCell align="right">Idade</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {alunos.map((aluno) => (
+                                        <TableRow
+                                            key={aluno.id.value}
+                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                        >
+                                            <TableCell align="right">{aluno.id.value}</TableCell>
+                                            <TableCell align="right">{aluno.name.first}</TableCell>
+                                            <TableCell align="right">{aluno.dob.age}</TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    )
+                }
+
             </Container>
         </>
     );
