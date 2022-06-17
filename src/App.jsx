aa6d542dd447f70ac307a1e9740a732431eb1b5f
Leonardo Aguiar <leonardoaguiar1235@gmail.com>
2022-06-17 20:22:57 -0300

    React

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,33 @@ import ArmazemSerratec from "./pages/ArmazemSerratec";
 import Alunos from "./pages/Alunos";
 import { CadastroMaterias } from "./pages/CadastroMaterias";
 import { ListagemMaterias } from "./pages/ListagemMaterias";
+import NavBar from "./components/Navbar";
+import { TemaContext } from "./context"
+import { useContext } from "react";
+
 function App() {
+  const { temaSelecionado } = useContext(TemaContext);
+  const tema = {
+    claro: {
+      backgroundColor: '#fff'
+    },
+    escuro: {
+      backgroundColor: '#363535'
+    }
+  }
+
   return (
-   <div className='container'>
-       <Routes>
-        <Route path="/" element={<ArmazemSerratec />} />
-        <Route path="alunos" element={<Alunos />} />
-        <Route path="/materias">
-          <Route path="cadastro" element={<CadastroMaterias/>} />
-          <Route path="listagem" element={<ListagemMaterias/>} />
-        </Route>
-      </Routes>
-   </div>
+    <div className='container' style={tema[temaSelecionado]}>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<ArmazemSerratec />} />
+          <Route path="alunos" element={<Alunos />} />
+          <Route path="/materias">
+            <Route path="cadastro" element={<CadastroMaterias />} />
+            <Route path="listagem" element={<ListagemMaterias />} />
+          </Route>
+        </Routes>
+    </div>
   );
 }
 
--- src/pages/Alunos/index.jsx
+++ src/pages/Alunos/index.jsx
@@ -1,10 +1,4 @@
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -17,7 +11,7 @@ import axios from "axios";
 import { useEffect, useState } from 'react';
 import Lottie from 'react-lottie';
 import carregandoAnimacao from '../../animations/loading.json';
-import { MyLink } from "../../Styles";
+
 
 const Alunos = () => {
     const [alunos, setAlunos] = useState([]);
@@ -54,27 +48,7 @@ const Alunos = () => {
 
     return (
         <>
-            <Box sx={{ flexGrow: 1 }}>
-                <AppBar position="static" sx={{ background: "#ff5555" }}>
-                    <Toolbar>
-                        <IconButton
-                            size="large"
-                            edge="start"
-                            color="inherit"
-                            aria-label="menu"
-                            sx={{ mr: 2 }}
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                            News
-                        </Typography>
-                        <Button color="inherit">
-              <MyLink to="/materias/cadastro">Login</MyLink>
-            </Button>
-                    </Toolbar>
-                </AppBar>
-            </Box>
+           
 
             <Container maxWidth="sm">
                 {
